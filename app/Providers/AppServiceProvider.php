3c817071d3e2d927f7cbe1d80fa1306049d03a56
Ariya Rahman <ariya.rahman@g.bracu.ac.bd>
2025-12-18 22:57:56 +0600

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,16 +2,6 @@
 
 namespace App\Providers;
 
-use App\Models\Listing;
-use App\Models\Booking;
-use App\Policies\ListingPolicy;
-use App\Policies\BookingPolicy;
-use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-
-<?php
-
-namespace App\Providers;
-
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -32,3 +22,4 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 }
+
