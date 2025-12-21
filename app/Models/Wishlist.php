bb038a1405f1eec22b56959abfb554b93ac3b52e
Ariya Rahman <ariya.rahman@g.bracu.ac.bd>
2025-12-21 23:53:31 +0600

    Update Wishlist.php

--- app/Models/Wishlist.php
+++ app/Models/Wishlist.php
@@ -1,1 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class User extends Authenticatable
+{
+    // ... other properties and methods
+
+    /**
+     * Relationship: a user can have many wishlist items.
+     */
+    public function wishlist(): HasMany
+    {
+        return $this->hasMany(Wishlist::class);
+    }
+}
 
