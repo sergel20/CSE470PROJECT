2d734348ad701c3081cef57c39c9a15cb3ac5dd3
Ariya Rahman <ariya.rahman@g.bracu.ac.bd>
2026-01-05 00:46:29 +0600

    Update 20125_11_00_notification_table.php

--- database/migrations/database/migrations/20125_11_00_notification_table.php
+++ database/migrations/database/migrations/20125_11_00_notification_table.php
@@ -1,1 +1,9 @@
+Schema::create('notifications', function (Blueprint $table) {
+    $table->uuid('id')->primary();
+    $table->string('type');
+    $table->morphs('notifiable');
+    $table->text('data');
+    $table->timestamp('read_at')->nullable();
+    $table->timestamps();
+});
 
